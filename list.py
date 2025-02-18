6d3372e1b6c5f69c47c9344b43b7adae03178432
Pratibha-Chikkamat <prathibhachikkamath@gmail.com>
2025-02-18 23:13:01 +0530

    Practice

---
+++ list.py
@@ -0,0 +1,27 @@
+list=[10,20,30,40,50,60,70,80,90,100]
+for i in range(9,-1,-1):
+    print(list[i])
+
+list1=[10,20,30,40,50,60,70,80,90,100]
+for i in list1:
+    print(i)
+
+mylist=[10,20,30,40]
+for x in mylist:
+    print(x)
+
+list2=[10,20,30,40]
+list2[2]=300
+print(list2)
+list2.insert(0,200)
+print(list2)
+if list2==20:
+    print(list2)
+else:
+    print(0)
+list2.remove(300)
+print(list2)
+
+
+
+
