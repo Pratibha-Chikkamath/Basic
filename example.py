11797eb06de22c09fe7f6fb3326f0df1095b31ab
Pratibha-Chikkamat <prathibhachikkamath@gmail.com>
2025-02-28 19:33:47 +0530

    Practice

---
+++ example.py
@@ -0,0 +1,12 @@
+#How do you call a parent class constructor which has parameter x from the child class constructor show code
+class A:
+    def __init__(self,x):
+        self.x=x
+        print("parent class constructor called x=",x)
+class B(A):
+    def __init__(self,x,y):
+        super().__init__(x)
+        self.y=y
+        print("Child class constructor called y=",y)
+b=B(10,20)
+
